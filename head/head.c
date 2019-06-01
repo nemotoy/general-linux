691a8c13646a491933596449488374ca65518bb6
yuma-nemoto <yuma-nemoto@dac.co.jp>
2019-06-01 10:29:36 +0900

    Add a function of analyzing options to the head command

--- head/head.c
+++ head/head.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _GNU_SOURCE
+#include <getopt.h>
 
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+  {"lines", required_argument, NULL, 'n'},
+  {"help", no_argument, NULL, 'h'},
+  {0, 0, 0, 0}
+};
 
 int
 main(int argc, char *argv[])
 {
-  long nlines;
+  int opt;
+  long nlines = DEFAULT_N_LINES;
 
-  // コマンドライン引数の条件
-  if (argc <  2) {
-    fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
-    exit(1);
+  while((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+    switch (opt) {
+    case 'n':
+      nlines = atol(optarg);
+      break;
+    case 'h':
+      fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+      exit(0);
+    case '?':
+      fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+      exit(1);
+    }
   }
-  nlines = atol(argv[1]);
-  if (argc == 2 ) {
+
+  if (optind == argc) {
     do_head(stdin, nlines);
   } else {
     int i;
 
-    for (i = 2; i < argc; i++) {
+    for (i = optind; i < argc; i++) {
       FILE *f;
 
       f = fopen(argv[i], "r");
-      // equals f == NULL
       if (!f) {
         perror(argv[i]);
         exit(1);
