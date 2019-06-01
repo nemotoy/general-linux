f18bec2a37a8543178bcf4409c2138c12ef67dc3
yuma-nemoto <yuma-nemoto@dac.co.jp>
2019-06-01 09:44:10 +0900

    Enable output lines from an indicated filename

--- head/head.c
+++ head/head.c
@@ -6,11 +6,32 @@ static void do_head(FILE *f, long nlines);
 int
 main(int argc, char *argv[])
 {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
+  long nlines;
+
+  // コマンドライン引数の条件
+  if (argc <  2) {
+    fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
     exit(1);
   }
-  do_head(stdin, atol(argv[1]));
+  nlines = atol(argv[1]);
+  if (argc == 2 ) {
+    do_head(stdin, nlines);
+  } else {
+    int i;
+
+    for (i = 2; i < argc; i++) {
+      FILE *f;
+
+      f = fopen(argv[i], "r");
+      // equals f == NULL
+      if (!f) {
+        perror(argv[i]);
+        exit(1);
+      }
+      do_head(f, nlines);
+      fclose(f);
+    }
+  }
   exit(0);
 }
 
@@ -20,6 +41,7 @@ do_head(FILE *f, long nlines)
   int c;
 
   if (nlines <= 0) return;
+  // \nを探して行を把握する
   while ((c = getc(f)) != EOF) {
     if (putchar(c) < 0) exit(1);
     if (c == '\n') {
